b5a74acd42ddfe65166ed96b151600d578b5048d
Victor Szoltysek <victor.szoltysek@flightnetwork.com>
2017-04-17 20:16:49 -0400

    Adding Validation of Deployment and Environment Names.

--- src/main/java/com/example/bamboo/DeployTaskConfigurator.java
+++ src/main/java/com/example/bamboo/DeployTaskConfigurator.java
@@ -1,14 +1,27 @@
 package com.example.bamboo;
 
 import com.atlassian.bamboo.collections.ActionParametersMap;
+import com.atlassian.bamboo.deployments.environments.Environment;
+import com.atlassian.bamboo.deployments.projects.DeploymentProject;
+import com.atlassian.bamboo.deployments.projects.service.DeploymentProjectService;
 import com.atlassian.bamboo.task.AbstractTaskConfigurator;
 import com.atlassian.bamboo.task.TaskDefinition;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.List;
 import java.util.Map;
 
 public class DeployTaskConfigurator extends AbstractTaskConfigurator {
+
+    @ComponentImport
+    private final DeploymentProjectService deploymentProjectService;
+
+    public DeployTaskConfigurator(DeploymentProjectService deploymentProjectService)
+    {
+        this.deploymentProjectService = deploymentProjectService;
+    }
 
     public Map<String, String> generateTaskConfigMap(final ActionParametersMap params, final TaskDefinition previousTaskDefinition)
     {
@@ -32,13 +45,49 @@ public class DeployTaskConfigurator extends AbstractTaskConfigurator {
     {
         super.validate(params, errorCollection);
 
-        final String environment = params.getString("environment");
-        final String deploymentProject = params.getString("deploymentProject");
+        final String deploymentProjectName = params.getString("deploymentProject");
+        final String environmentName = params.getString("environment");
 
-        if (StringUtils.isEmpty(environment))
+        if (StringUtils.isEmpty(deploymentProjectName))
+            errorCollection.addError("deploymentProject", "Deployment Project cannot be empty.");
+
+        if (StringUtils.isEmpty(environmentName))
             errorCollection.addError("environment", "Environment can not be empty.");
 
-        if (StringUtils.isEmpty(deploymentProject))
-            errorCollection.addError("deploymentProject", "Deployment Project cannot be empty.");
+        if (StringUtils.isEmpty(environmentName) || StringUtils.isEmpty(deploymentProjectName))
+            return;
+
+        DeploymentProject deploymentProject = getMatchingDeploymentProject(deploymentProjectName);
+
+        if (deploymentProject == null){
+            errorCollection.addError("deploymentProject", "Unable to find Deployment Project named: " + deploymentProjectName);
+            return;
+        }
+
+        if(getMatchingEnvironment(deploymentProject, environmentName) == null)
+            errorCollection.addError("environment", "Unable to find Environment named: " + environmentName + " for deployment project : " + deploymentProjectName);
+
+    }
+
+    private DeploymentProject getMatchingDeploymentProject(String name){
+
+        List<DeploymentProject> allDeploymentProjects = deploymentProjectService.getAllDeploymentProjects();
+
+        for (DeploymentProject deploymentProject : allDeploymentProjects) {
+            if(deploymentProject.getName().equals(name))
+                return deploymentProject;
+        }
+
+        return null;
+    }
+
+    private Environment getMatchingEnvironment(DeploymentProject deploymentProject, String name) {
+
+        for (Environment environment : deploymentProject.getEnvironments()) {
+            if(environment.getName().equals(name))
+                return environment;
+        }
+
+        return null;
     }
 }
